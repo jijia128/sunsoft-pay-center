7aff446a3df08d961d49a58f23b6c48d44858073
Your Name <you@example.com>
2018-09-26 16:31:25 +0800

    配置文件修改

--- config-center/src/main/java/com/sunsoft/config/ConfigServer.java
+++ config-center/src/main/java/com/sunsoft/config/ConfigServer.java
@@ -9,11 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServer {
-
          public static void main(String[] args) {
-
         	 SpringApplication.run(ConfigServer.class, args);
-
          }
-
 }
