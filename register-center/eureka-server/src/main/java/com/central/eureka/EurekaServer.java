028f9479fe9b6bb9a4055dc5e327a1464ff31cb3
Your Name <you@example.com>
2018-09-28 16:02:36 +0800

    添加打包环境配置

--- register-center/eureka-server/src/main/java/com/central/eureka/EurekaServer.java
+++ register-center/eureka-server/src/main/java/com/central/eureka/EurekaServer.java
@@ -1,5 +1,6 @@
 package com.central.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -18,16 +19,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @EnableHystrixDashboard
 @EnableTurbine
 public class EurekaServer {
-
 	public static void main(String[] args) {
-
-//    	1本地启动采用此方法加载profiles文件
-//		ConfigurableApplicationContext context = new SpringApplicationBuilder(UnieapEurekaServerApplication.class).
-//				profiles("slave3").run(args);
-//    	2服务器采用此方法 java -jar   --spring.profiles.active=slave3;
-//    	 SpringApplication.run(DreiEurekaServerApp.class, args);
-    	ConfigurableApplicationContext context = new SpringApplicationBuilder(EurekaServer.class).
-				profiles("slave0").run(args);
+		SpringApplication.run(EurekaServer.class, args);
 	}
-
 }
