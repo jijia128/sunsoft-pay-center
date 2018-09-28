4d0cc1601f8e5894be35d007309353de00db0043
Your Name <you@example.com>
2018-09-28 15:08:42 +0800

    添加打包环境配置

--- job-center/job-admin/src/main/java/com/xxl/job/admin/JobAdminServer.java
+++ job-center/job-admin/src/main/java/com/xxl/job/admin/JobAdminServer.java
@@ -2,9 +2,7 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -15,8 +13,8 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @ComponentScan(basePackages={"com.xxl.job.admin","com.central"})
 @PropertySource(value = "classpath:xxl-job-admin.properties")
-public class Application {
+public class JobAdminServer {
     public static void main(String[] args){
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(JobAdminServer.class,args);
     }
 }
