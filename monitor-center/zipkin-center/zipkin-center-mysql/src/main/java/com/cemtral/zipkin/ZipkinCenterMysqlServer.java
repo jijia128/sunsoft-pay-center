003ae4d3aaf04d6cf4a564f6dc228ff7c6455532
everher <jijia128@163.com>
2018-10-11 23:06:58 +0800

    开发基线

--- monitor-center/zipkin-center/zipkin-center-mysql/src/main/java/com/cemtral/zipkin/ZipkinCenterMysqlServer.java
+++ monitor-center/zipkin-center/zipkin-center-mysql/src/main/java/com/cemtral/zipkin/ZipkinCenterMysqlServer.java
@@ -2,10 +2,11 @@ package com.cemtral.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.internal.EnableZipkinServer;
 
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 @EnableZipkinServer        //默认采用HTTP通信方式启动ZipkinServer
 public class ZipkinCenterMysqlServer {
